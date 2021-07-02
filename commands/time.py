e9c9c33addfcd7f1f25fc3378c3f30a04bb2ae86
Kamil <40839443+kamil34@users.noreply.github.com>
2021-07-02 11:14:41 +0200

    time command update

--- commands/time.py
+++ commands/time.py
@@ -3,7 +3,12 @@ from discord.ext import commands
 import datetime
 import config.settings
 
-get_time = datetime.datetime.now()
+datetime = datetime.datetime.now()
+date = datetime.strftime("%m/%d/%Y")
+clock = (datetime.strftime("%H:%M:%S %p").replace('AM', 'a.m.').replace('PM', 'p.m.'))
+
+
 @config.settings.bot.command()
 async def time(ctx):
-    await ctx.send('Current time is: {}'.format(get_time))
+    await ctx.send('Current date is: {}'.format(date))
+    await ctx.send('Current time is: {}'.format(clock))
